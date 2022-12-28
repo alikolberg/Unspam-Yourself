b0835f3ff65556b2a2e4a9cf1474916064c592a9
alison kolberg <alisonjkolberg@gmail.com>
2022-12-28 13:45:21 -0800

    updated welcome

--- uy-app/src/index.js
+++ uy-app/src/index.js
@@ -11,7 +11,10 @@ root.render(
     <App />
   </React.StrictMode>
 );
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<Welcome />);
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
