652fc42c21974af5d15bf4ce8a7616b10e1c71eb
alison kolberg <alisonjkolberg@gmail.com>
2022-12-28 14:12:10 -0800

    update commet

--- uy-app/src/index.js
+++ uy-app/src/index.js
@@ -5,12 +5,12 @@ import App from './App';
 import Welcome from './Welcome';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//  <React.StrictMode>
+//    <App />
+//  </React.StrictMode>
+// );
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<Welcome />);
