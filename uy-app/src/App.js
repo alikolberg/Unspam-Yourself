caac6b919ce1ae5a8af48a5a52195822d87e6b96
alison kolberg <alisonjkolberg@gmail.com>
2022-12-28 16:50:14 -0800

    update for button

--- uy-app/src/App.js
+++ uy-app/src/App.js
@@ -1,16 +1,28 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Button from './components/Button';
 
-import {Home} from './components/Home'
-
-import Button from 'react-bootstrap/Button'
-
-function App () {
+function App() {
   return (
-    <div className="container">
-      <Home/>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+        <Button/>
+      </header>
     </div>
   );
 }
+
 export default App;
