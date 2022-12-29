ae26c1cb2306a7b22b2339a187faad0ad34d5e91
alison kolberg <alisonjkolberg@gmail.com>
2022-12-28 16:43:44 -0800

    update button and index

---
+++ uy-app/src/components/Button.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function Button() {
+  return (
+    <div className="App">
+        <button>Submit</button>
+    </div>
+  );
+}
+
+export default Button;
