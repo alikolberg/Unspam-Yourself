a69632beae1a23670dbfb437571a9fbca21d01ea
alison kolberg <alisonjkolberg@gmail.com>
2022-12-28 17:37:40 -0800

    update search

---
+++ uy-app/src/components/Search.js
@@ -0,0 +1,38 @@
+import logo from './logo.svg';
+import './App.css';
+
+
+function Search() {
+    return (
+      <div className="App">
+          <ReactSearchBox
+            placeholder="Search for an organization"
+            data={[
+            {
+            key: "Make a wish foundation",
+            value: "Make a Wish Foundation"
+            },
+            {
+                key: "World Wildlife Fund",
+                value: "World Wildlife Fund"
+            },
+            {
+                key: "Wounded Warriro Project",
+                value: "Wounded Warriro Project"
+            }
+            ]}
+            onSelect={(record: any) => console.log(record)}
+            onFocus={() => {
+            console.log("This function is called when is focussed");
+            }}
+            onChange={(value) => console.log(value)}
+            autoFocus
+            leftIcon={<>🔎</>}
+            iconBoxSize="48px"
+            />
+      </div>
+    );
+  }
+  
+  export default Search;
+
